719823a5718a09e4369a56eef2088f314569ca88
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2022-01-24 13:04:43 -0300

    FEAT: [Trybe Tunes] - Requeriment 5 :robot:

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,53 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchBox: '',
+      conditionButton: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      searchBox: value,
+    },
+    this.setState({
+      conditionButton: this.changeButton(),
+    }));
+  }
+
+  changeButton = () => {
+    const { searchBox } = this.state;
+    const min = 1;
+    if (searchBox.length < min) return true;
+    return false;
+  }
+
   render() {
+    const { searchBox, conditionButton } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Bon Jour</p>
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            name="searchBox"
+            value={ searchBox }
+            onChange={ this.handleChange }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ conditionButton }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
