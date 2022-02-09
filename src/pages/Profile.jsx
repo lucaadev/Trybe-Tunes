6d8239a905eef31931d1357a84c0a08668998fb3
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2022-02-09 17:16:38 -0300

    Feat: [Trybe Tunes] - Requeriment 13 :robot:

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
 import Header from './components/Header';
+import Loading from './components/Loading';
 
 export default class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      infoProfile: {},
+      load: true,
+    };
+  }
+
+  componentDidMount() {
+    this.getInfoLogin();
+  }
+
+  getInfoLogin = async () => {
+    const getInfo = await getUser();
+    this.setState({
+      infoProfile: getInfo,
+      load: false,
+    });
+  }
+
   render() {
+    const { infoProfile, load } = this.state;
+    if (load) {
+      return (
+        <>
+          <Header />
+          <Loading />
+        </>
+      );
+    }
     return (
-      <div data-testid="page-profile">
+      <>
         <Header />
-        <p> Profile</p>
-      </div>
+        <div data-testid="page-profile">
+          <img data-testid="profile-image" src={ infoProfile.image } alt="profile" />
+          <p>{infoProfile.name}</p>
+          <p>{infoProfile.name}</p>
+          <p>{infoProfile.email}</p>
+          <p>{infoProfile.description}</p>
+          <Link to="/profile/edit">
+            Editar perfil
+          </Link>
+        </div>
+      </>
     );
   }
 }
